a5e8a4f7c7fe94107978090d923a75f861760621
avanishdidwania <avanishdidwania@gmail.com>
2026-03-27 15:15:01 +0530

    Add protected route and fix routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
     return (
@@ -10,8 +11,11 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/dashboard" element={<ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>} />
                 </Routes>
             </Router>
         </AuthProvider>
